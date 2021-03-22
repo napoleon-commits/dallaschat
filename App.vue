5acab0050cd157bd117e869ac302ceb32712037e
napoleon-commits <62509875+napoleon-commits@users.noreply.github.com>
2021-03-22 11:14:46 -0500

    Initial commit

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{message}}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message: "Selma Chat; a texting app made for residents of Selma, Alabama.",
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>

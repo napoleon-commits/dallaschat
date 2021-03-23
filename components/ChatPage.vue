dc1022927c8d3004347672e67b84d9849b0f68ea
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2021-03-23 10:36:39 -0500

    beta

--- components/ChatPage.vue
+++ components/ChatPage.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <div style="text-align: center; font-size: 1.5em;">Your Username: {{username}}</div>
         <div id="chat-box">
             <br />
             <br />
@@ -30,24 +31,68 @@
             <br />
         </div>
         <div id="toolbar">
-            <input style="width: 48%;" type="text" :placeholder="`Receiver's Username`"/><br />
-            <input style="width: 48%;" type="text" placeholder="Type message here:"/><br/>
-            <button>Send</button>
+            <input
+                v-model="receiver"
+                style="width: 99%;"
+                type="text"
+                :placeholder="`Receiver's Username`"
+                v-on:keyup.enter="sendMessage"
+            /><br />
+            <input
+                v-model="message"
+                style="width: 99%;"
+                type="text"
+                placeholder="Type message here:"
+                v-on:keyup.enter="sendMessage"
+            /><br/>
+            <button @click="sendMessage">Send</button>
         </div>
     </div>
 </template>
 
 <script>
-import MockChat from '../mocks/MockChat';
+import PrivateVariables from '../PrivateVariables';
 
 export default {
     name: 'ChatPage',
     props: ['username'],
     data(){
         return {
-            chat: MockChat,
+            chat: [],
+            connection: null,
+            PrivateVariables,
+            receiver: '',
+            message: '',
         };
-    }
+    },
+    created(){
+        this.connection = new WebSocket(`${PrivateVariables.wssURL}${this.username}`);
+        this.connection.onmessage = (event) => {
+            const dataObject = JSON.parse(event.data);
+            this.chat.push({
+                username: dataObject.sender,
+                message: dataObject.message,
+            });
+        }
+    },
+    methods: {
+        sendMessage: function() {
+            if(this.message.length > 0 && this.receiver.length > 0){
+                const messageObject = {
+                    sender: this.username,
+                    receiver: this.receiver,
+                    message: this.message,
+                    action: "message",
+                };
+                this.connection.send(JSON.stringify(messageObject));
+                this.chat.push({
+                    username: `${this.username}`,
+                    message: this.message,
+                });
+                this.message = '';
+            }
+        }
+    },
 }
 </script>
 

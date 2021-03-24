da1bea9a468f6f5b4ac6a7909e9dff136c1f3814
Byron Hall <byronhall@MacBook-Pro.local>
2021-03-23 23:32:09 -0500

    updated build process

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,21 @@ const { VueLoaderPlugin } = require('vue-loader');
 const { HotModuleReplacementPlugin } = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const plugins = [
+    new HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HTMLWebpackPlugin({
+        showErrors: true,
+        cache: true,
+        template: join(__dirname, 'index.html')
+    }),
+];
+
+if(process.env.WEBPACK_DEV_SERVER) {
+    plugins.push(new BundleAnalyzerPlugin(),);
+}
+
 module.exports = {
     entry: join(__dirname, 'app.js'), 
     output: {
@@ -30,16 +45,7 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new HotModuleReplacementPlugin(),
-        new VueLoaderPlugin(),
-        new HTMLWebpackPlugin({
-            showErrors: true,
-            cache: true,
-            template: join(__dirname, 'index.html')
-        }),
-        new BundleAnalyzerPlugin(),
-    ],
+    plugins,
     resolve: {
         alias: {
           'vue$': 'vue/dist/vue.esm.js'
